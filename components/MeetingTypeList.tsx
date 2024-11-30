96ee127edf0099fb81a3699bff932af76b1689bf
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-30 16:42:03 +0100

    feat: Add MeetingModal to MeetingTypeList
    Added the MeetingModal component to the MeetingTypeList component, allowing users to create an instant meeting when they click on the "New meeting" card. The MeetingModal component is used to display a dialog box with a title, description, and a button to create the meetin

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
 import HomeCard from './HomeCard'
+import MeetingModal from './MeetingModal'
 
 
 const MeetingTypeList = () => {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const [meetingState, setMetingState] = useState<'isJoiningMeeting'|'isScheduleMeeting'|'isInstantMeeting' | undefined>()
+const createMeeting = () => {};
+
     return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4'>
         <HomeCard 
             img='/icons/add-meeting.svg'
             title='New meeting'
             description='Instantly start a meeting'
-            handleClick={()=> setMetingState('isJoiningMeeting')}
+            handleClick={()=> setMetingState('isInstantMeeting')}
             className='bg-orange-1'
         />
         <HomeCard 
@@ -37,6 +40,14 @@ const [meetingState, setMetingState] = useState<'isJoiningMeeting'|'isScheduleMe
             handleClick={()=> setMetingState('isJoiningMeeting')}
             className='bg-yellow-1'
 
+        />
+        <MeetingModal 
+            isOpen={meetingState === 'isInstantMeeting'}
+            onClose={()=> setMetingState(undefined)}
+            title='Start an instant meeting'
+            className='text-center'
+            buttonText='Create a Meet Sphere'
+            handleClick={createMeeting}
         />
     </section>
     )
