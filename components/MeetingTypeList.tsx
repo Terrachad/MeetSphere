4b59844f3a891ea5e9abd9a1767059e9211eb549
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-30 15:12:15 +0100

    Create MeetingTypeList.tsx

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetingTypeList = () => {
+  return (
+    <section className='grid grid-cols-1 gpa-5 md:grid-cols-2 xl:grids-col-4'>
+        
+    </section>
+  )
+}
+
+export default MeetingTypeList
