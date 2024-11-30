a6b879d6595540b90bcdcb9e4a4e9ad2ae199daf
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-30 15:52:45 +0100

    feat: Add HomeCardProps type
    Added a new type, HomeCardProps, to the app/index.ts file, which represents the props for a HomeCard component.

---
+++ app/index.ts
@@ -0,0 +1,7 @@
+export declare type HomeCardProps = {
+    img:string,
+    title: string,
+    description: string,
+    handleClick: () => void;
+    className: string;
+}
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import HomeCard from './HomeCard'
 
 const MeetingTypeList = () => {
-  return (
-    <section className='grid grid-cols-1 gpa-5 md:grid-cols-2 xl:grids-col-4'>
-        
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const [meetingState, setMetingState] = useState<'isJoiningMeeting'|'isScheduleMeeting'|'isInstantMeeting' | undefined>()
+    return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4'>
+        <HomeCard 
+            img='/icons/add-meeting.svg'
+            title='New meeting'
+            description='Instantly start a meeting'
+            handleClick={()=> setMetingState('isJoiningMeeting')}
+            className='bg-orange-1'
+        />
+        <HomeCard 
+            img='/icons/schedule.svg'
+            title='Schedule meeting'
+            description='Plan your meeting'
+            handleClick={()=> setMetingState('isScheduleMeeting')}
+            className='bg-blue-1'
+
+        />
+        <HomeCard 
+            img='/icons/recordings.svg'
+            title='View Recordings'
+            description='Check out recorded meets'
+            handleClick={()=> setMetingState('isJoiningMeeting')}
+            className='bg-purple-1'
+
+        />
+        <HomeCard 
+            img='/icons/add-meeting.svg'
+            title='Join Meeting'
+            description='Join via link'
+            handleClick={()=> setMetingState('isJoiningMeeting')}
+            className='bg-yellow-1'
+
+        />
+
     </section>
-  )
+    )
 }
 
 export default MeetingTypeList
