b0866468a48a351e35ca766e25c255d93663f2d1
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-30 15:12:41 +0100

    feat: Update Home component to include MeetingTypeList and display meeting information
    Updated the Home component to include the MeetingTypeList component and display meeting information, such as the time and date of the upcoming meeting.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,15 +1,31 @@
 'use client'
 
+import MeetingTypeList from '@/components/MeetingTypeList'
 import React from 'react'
 
 const Home = () => {
+  const newDate = new Date()
+  const time = newDate.toLocaleTimeString('it-IT', {hour: '2-digit', minute: '2-digit'})
+  const date = (new Intl.DateTimeFormat('it-IT', {
+    dateStyle: 'full'
+  })).format(newDate)
   return (
-    <section
-      className='flex size-full flex-col gap-10 text-white'
-    >
+    <section className='flex size-full flex-col gap-10 text-white'>
         <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
-    
+          <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 md:p-8 lg:p-11'>
+            <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
+            Upcoming meeting at Nan</h2>
+                <div className='flex flex-col gap-2'>
+                    <h1 className='text-4xl font-extrabold lg:text-7xl'>
+                      {time}
+                    </h1>
+                    <p className='text-lg font-medium text-sky-1 lg:text-2xl'>
+                      {date}
+                    </p>
+                </div>
+          </div>
         </div>
+        <MeetingTypeList/>
     </section>
   )
 }
