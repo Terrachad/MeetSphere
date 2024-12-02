4b3241239782ac5493a579c4632c838fd52822bf
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-12-02 13:00:35 +0100

    feat: Introduced a custom MeetingSetup component that manages camera and microphone settings for a call using the useCall hook from Stream.io's video-react-sdk
    Implemented a custom MeetingSetup component that utilizes the useCall hook from Stream.io's video-react-sdk to manage the camera and microphone settings for a call. The component also includes a VideoPreview component to display a preview of the video feed.

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+'use client'
+
+import { useCall, VideoPreview } from '@stream-io/video-react-sdk'
+import React, { useEffect, useState } from 'react'
 
 const MeetingSetup = () => {
-  return (
-    <div>MeetingSetup</div>
+    const [isCamToggledOn, setIsCamToggledOn] = useState(false)
+    const call = useCall();
+    useEffect(() => {
+        if(isCamToggledOn){
+            call?.camera.disable();
+            call?.microphone.disable()
+        }
+        else{
+            call?.camera.enable();
+            call?.microphone.enable();
+        }
+    }, [isCamToggledOn, call?.camera, call?.microphone])
+    
+    return (
+    <div className='flex h-screen w-full flex-col items-center justify-center gpa-3 text-white'>
+            <h1 className='text-2xl font-bold'> 
+                <VideoPreview/>
+            </h1>
+    </div>
   )
 }
 
