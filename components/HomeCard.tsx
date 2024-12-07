a02aa2b1b4b874777795558259101e83e37eb3cf
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-12-07 19:10:04 +0100

    Update HomeCard.tsx

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const HomeCard = ({img, title, description, handleClick, className} : HomeCardProps) => {
     return (
-    <div className={` ${className} px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer`} onClick={handleClick}>
+    <div className={` ${className} px-4 py-6 flex flex-col justify-between w-full lg:max-w-[370px] min-h-[260px] rounded-[14px] cursor-pointer`} onClick={handleClick}>
         <div className='flex-center glassmorphism size-12 rounded-[10px]'>
             <Image
                 src={img}
